458c651cd4e0f0d40ca22bfa5cc036e8695ab9e3
red binder <redbinder0526@proton.me>
2026-04-21 21:41:44 +0200

    Fix conditional compilaton

--- rnex-core/src/nex/mod.rs
+++ rnex-core/src/nex/mod.rs
@@ -1,3 +1,5 @@
+use cfg_if::cfg_if;
+
 pub mod account;
 pub mod auth_handler;
 pub mod common;
@@ -6,4 +8,8 @@ pub mod matchmake;
 pub mod remote_console;
 pub mod user;
 pub mod datastore;
-pub mod s3presigner;
+cfg_if! {
+    if #[cfg(feature = "datastore")] {
+        pub mod s3presigner;
+    }
+}
--- rnex-core/src/nex/user.rs
+++ rnex-core/src/nex/user.rs
@@ -41,21 +41,37 @@ use rnex_core::prudp::socket_addr::PRUDPSockAddr;
 use rnex_core::rmc::response::ErrorCode::{Core_InvalidArgument, RendezVous_AccountExpired};
 use rnex_core::rmc::structures::qresult::QResult;
 use std::sync::{Arc, Weak};
+use cfg_if::cfg_if;
 use rnex_core::rmc::protocols::ranking::{CompetitionRankingScoreData, CompetitionRankingGetParam, CompetitionRankingScoreInfo};
 use rnex_core::rmc::structures::ranking::{UploadCompetitionData};
 use tokio::sync::{Mutex, RwLock};
 
-define_rmc_proto!(
-    proto UserProtocol{
-        Secure,
-        MatchmakeExtension,
-        MatchmakeExt,
-        Matchmake,
-        NatTraversal,
-        Ranking,
-        DataStore
+cfg_if! {
+    if #[cfg(feature = "datastore")] {
+        define_rmc_proto!(
+            proto UserProtocol{
+                Secure,
+                MatchmakeExtension,
+                MatchmakeExt,
+                Matchmake,
+                NatTraversal,
+                Ranking,
+                DataStore
+            }
+        );
+    } else {
+        define_rmc_proto!(
+            proto UserProtocol{
+                Secure,
+                MatchmakeExtension,
+                MatchmakeExt,
+                Matchmake,
+                NatTraversal,
+                Ranking
+            }
+        );
     }
-);
+}
 
 #[rmc_struct(UserProtocol)]
 pub struct User {
