678ec565a55b4d37dd62ce539dc81ecf736e5b8f
red binder <redbinder0526@proton.me>
2026-04-28 10:31:56 +0200

    basic buildkit output

--- .ci-scripts/make-edition.sh
+++ .ci-scripts/make-edition.sh
@@ -4,6 +4,7 @@ export EDITION=$1
 export BA="--network=host --build-arg EDITION=$1 --build-arg DATABASE_URL="$DATABASE_URL""
 source /etc/environment
 : "${RNEX_CONTAINER_PLATFORM:=podman}"
+export BUILDKIT_PROGRESS=plain
 
 # $RNEX_CONTAINER_PLATFORM build $BA -t "$CI_REGISTRY_IMAGE/$EDITION/dev-container:latest" --target=dev-container .
 # $RNEX_CONTAINER_PLATFORM push "$CI_REGISTRY_IMAGE/$EDITION/dev-container:latest"
