a75b155f6c2c6d46ca5b594ce570cbfdd0c4086f
Maple Nebel <maple.neb@gmail.com>
2026-05-29 11:09:07 +0200

    fix linking

--- build-edition.sh
+++ build-edition.sh
@@ -12,7 +12,7 @@ echo building $EDITION
 echo FEATURES:
 echo $EDITION_FEATURES
 
-if [[ -v RNEX_STATIC ]]; then
+if [[ ! -v RNEX_STATIC ]]; then
     cargo build --release --features "$EDITION_FEATURES"
 else
     OPENSSL_LIB_DIR=/usr/lib OPENSSL_INCLUDE_DIR=/usr/include/openssl OPENSSL_STATIC=1 RUSTFLAGS="-C relocation-model=static -C linker=ld.lld" cargo build --release --features "$EDITION_FEATURES" --target x86_64-unknown-linux-musl
