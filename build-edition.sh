25d58b30837b04a991c921764e44c77cbf1cd429
red binder <redbinder0526@proton.me>
2026-08-08 05:58:08 +0200

    attempt fix

--- build-edition.sh
+++ build-edition.sh
@@ -6,7 +6,6 @@ if [ -z ${EDITION+x} ]; then
     EDITION=$1
 fi
 
-# comma seperated list of features for the specified version
 source ./buildscripts/common.sh
 echo building $EDITION
 echo FEATURES:
@@ -15,5 +14,5 @@ echo $EDITION_FEATURES
 if [[ ! -v RNEX_STATIC ]]; then
     cargo build --release --features "$EDITION_FEATURES"
 else
-    OPENSSL_LIB_DIR=/usr/lib OPENSSL_INCLUDE_DIR=/usr/include/openssl OPENSSL_STATIC=1 RUSTFLAGS="-C relocation-model=static -C linker=ld.lld" cargo build --release --features "$EDITION_FEATURES" --target x86_64-unknown-linux-musl
+    CC_x86_64_unknown_linux_musl=musl-gcc OPENSSL_LIB_DIR=/usr/lib/x86_64-linux-gnu OPENSSL_INCLUDE_DIR=/usr/include OPENSSL_STATIC=1 RUSTFLAGS="-C relocation-model=static -C linker=ld.lld" cargo build --release --features "$EDITION_FEATURES" --target x86_64-unknown-linux-musl
 fi
