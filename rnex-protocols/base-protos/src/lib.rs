be7bc79762cfcff2d11495c2f4ae862393c0f997
red binder <redbinder0526@proton.me>
2026-08-08 19:01:16 +0200

    fix(resultsrange): use usize min instead of usize max

--- rnex-protocols/base-protos/src/lib.rs
+++ rnex-protocols/base-protos/src/lib.rs
@@ -5,6 +5,7 @@ pub mod secure;
 pub mod util;
 use secure::{RawSecure, RawSecureInfo, RemoteSecure, Secure};
 use util::{RawUtility, RawUtilityInfo, RemoteUtility, Utility};
+use cfg_if::cfg_if;
 
 #[derive(RmcSerialize, Debug, Default, Clone)]
 #[rmc_struct(0)]
@@ -13,13 +14,26 @@ pub struct ResultsRange {
     pub size: u32,
 }
 
-impl ResultsRange {
-    pub fn make_from_list<T: Clone>(&self, list: &[T]) -> Vec<T> {
-        let end = usize::max(list.len(), self.offset as usize + self.size as usize);
+cfg_if! {
+    if #[cfg(feature = "v3-8-13")] {
+        impl ResultsRange {
+            pub fn make_from_list<T: Clone>(&self, list: &[T]) -> Vec<T> {
+                let start = (self.offset as usize).min(list.len());
+                let end = (start + self.size as usize).min(list.len());
 
-        list.get(self.offset as usize..end)
-            .unwrap_or_default()
-            .into()
+                list[start..end].to_vec()
+            }
+        }
+    } else {
+        impl ResultsRange {
+            pub fn make_from_list<T: Clone>(&self, list: &[T]) -> Vec<T> {
+                let end = usize::max(list.len(), self.offset as usize + self.size as usize);
+
+                list.get(self.offset as usize..end)
+                    .unwrap_or_default()
+                    .into()
+            }
+        }
     }
 }
 
