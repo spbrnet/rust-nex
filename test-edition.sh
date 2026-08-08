bfc14823281fbbb7c0d613c4a281eee19bba8e9e
red binder <redbinder0526@proton.me>
2026-08-08 06:24:45 +0200

    i dont like spamming commits but its the only way to test ci

--- test-edition.sh
+++ test-edition.sh
@@ -11,4 +11,4 @@ source ./buildscripts/common.sh
 echo FEATURES:
 echo $EDITION_FEATURES
 
-PKG_CONFIG_ALLOW_CROSS=1 OPENSSL_DIR=/usr cargo build --release --features "$EDITION_FEATURES" --target x86_64-unknown-linux-musl
+PKG_CONFIG_ALLOW_CROSS=1 OPENSSL_INCLUDE_DIR="/usr/include -I/usr/include/x86_64-linux-gnu" OPENSSL_LIB_DIR=/usr/lib/x86_64-linux-gnu cargo build --release --features "$EDITION_FEATURES" --target x86_64-unknown-linux-musl
